b734b60d4d314fdec2ee098704f2680aac1a2a52
Baby Gulnasheen <babygulnasheen9922@gmail.com>
2026-04-22 14:07:24 +0530

    Fix login cookie issue

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -24,8 +24,8 @@ export const auth = betterAuth({
         name: 'auth_session',
         attributes:{
             httpOnly:true,
-            secure:process.env.NODE_ENV==='production',
-            sameSite:process.env.NODE_ENV==='production'?'none':'lax',
+            secure: true,
+            sameSite: 'none',
             path:'/',
         }
        }
